15456e2c03bb2319f2c36ff8bb3d3e8382662242
novakw267 <novakw267@gmail.com>
2017-03-20 14:35:20 -0400

    This is the ember routing code
    Committing my structured ember routes, through the lab.
    I am pushing this so I have a reference during the project.
    Hopefully ember will be helpful for structuring my front end.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function () {
+  this.route('about');
+  this.route('team', function(){
+    this.route('engineering');
+    this.route('leadership');
+  });
+  this.route('contact', function(){
+    this.route('nyc');
+    this.route('boston');
+  });
 });
 
 export default Router;
